3864ab93cee5ee4fdd3057bbc5b9cdc2f7bcf03a
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-05-23 22:36:28 +0700

    update init mongodb

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017', {
-        dbName: "auth_tutorial"
+        dbName: 'auth_tutorial',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
     .then(() => {
         console.log("Mongodb connected")
